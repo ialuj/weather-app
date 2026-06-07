351081ef97db667dc65f1362c872696974a850b4
ialuj <ritsure.julai.jose@gmail.com>
2026-06-08 00:57:51 +0200

    Frontend updates

--- weather-info-frontend/src/environments/environment.prod.ts
+++ weather-info-frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl:
-    process.env['BACKEND_URL'] || 'https://weather-backend.onrender.com/api',
+    process.env['BACKEND_URL'] || 'https://weather-info-api.onrender.com/api',
 };
