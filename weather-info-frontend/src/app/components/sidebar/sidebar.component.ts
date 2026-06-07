474b3c9723875036f35c1ea9fd2fc5e832a2de8e
ialuj <ritsure.julai.jose@gmail.com>
2026-06-08 00:34:46 +0200

    Frontend updates

--- weather-info-frontend/src/app/components/sidebar/sidebar.component.ts
+++ weather-info-frontend/src/app/components/sidebar/sidebar.component.ts
@@ -1,20 +1,49 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 export type MenuOption = 'cities' | 'location' | 'others';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss',
+  styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent {
   @Output() menuChange = new EventEmitter<MenuOption>();
+
   activeMenu: MenuOption = 'cities';
+  isOpen = false;
+  isMobile = false;
+
+  ngOnInit() {
+    this.checkScreenSize();
+  }
+
+  @HostListener('window:resize')
+  checkScreenSize() {
+    this.isMobile = window.innerWidth <= 768;
+    if (!this.isMobile) {
+      this.isOpen = false;
+    }
+  }
 
   setActive(option: MenuOption) {
     this.activeMenu = option;
     this.menuChange.emit(option);
+    if (this.isMobile) {
+      this.isOpen = false;
+    }
+  }
+
+  toggleSidebar() {
+    this.isOpen = !this.isOpen;
+  }
+
+  closeSidebar() {
+    if (this.isMobile) {
+      this.isOpen = false;
+    }
   }
 }
